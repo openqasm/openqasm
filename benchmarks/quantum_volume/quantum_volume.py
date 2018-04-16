d53b089d4609e1b1511f0026e5c1b2c2d472af4a
Ali Javadi <ajavadia@princeton.edu>
2018-04-16 16:42:09 -0400

    clean up quantum volume script

--- benchmarks/quantum_volume/quantum_volume.py
+++ benchmarks/quantum_volume/quantum_volume.py
@@ -1,92 +1,86 @@
 """
-Generate randomized circuits for Quantum Volume analysis.
+Generate (or run) randomized circuits for Quantum Volume analysis.
 
 Example run:
-  python RandomCircuits.py -n 5 -d 5
+  python quantum_volume.py -n 5 -d 5
 """
 
-import sys
 import math
-import time
 import argparse
-import numpy as np
-from scipy.linalg import qr
-from scipy.linalg import det
-from qiskit import QuantumProgram
+from numpy import random
+from scipy import linalg
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
 from qiskit.mapper import two_qubit_kak
-
-if sys.version_info < (3, 0):
-    raise Exception("Please use Python version 3 or greater.")
+from qiskit.wrapper import register, execute, get_backend, compile
 
 
 def random_SU(n):
-    """Return an n x n Haar distributed unitary matrix.
-    Return numpy array.
+    """Return an n x n Haar distributed unitary matrix,
+    using QR-decomposition on a random n x n.
     """
-    X = (np.random.randn(n, n) + 1j*np.random.randn(n, n))
-    # Q is a unitary matrix
-    Q, R = qr(X)
-    # make Q a special unitary
-    Q /= pow(det(Q), 1/n)
+    X = (random.randn(n, n) + 1j * random.randn(n, n))
+    Q, R = linalg.qr(X)           # Q is a unitary matrix
+    Q /= pow(linalg.det(Q), 1/n)  # make Q a special unitary
     return Q
 
 
-def build_model_circuits(name, n, depth, num_circ=1):
+def build_model_circuits(n, depth, num_circ=1):
     """Create a quantum program containing model circuits.
     The model circuits consist of layers of Haar random
     elements of SU(4) applied between corresponding pairs
     of qubits in a random bipartition.
-    name = leading name of circuits
-    n = number of qubits
-    depth = ideal depth of each model circuit (over SU(4))
-    num_circ = number of model circuits to construct
-    Return a quantum program.
+
+    Args:
+        n (int): number of qubits
+        depth (int): ideal depth of each model circuit (over SU(4))
+        num_circ (int): number of model circuits to construct
+
+    Returns:
+        list(QuantumCircuit): list of quantum volume circuits
     """
-    qp = QuantumProgram()
-    q = qp.create_quantum_register("q", n)
-    c = qp.create_classical_register("c", n)
-    # Create measurement subcircuit
-    meas = qp.create_circuit("meas", [q], [c])
-    for j in range(n):
-        meas.measure(q[j], c[j])
+    # Create quantum/classical registers of size n
+    q = QuantumRegister(n)
+    c = ClassicalRegister(n)
     # For each sample number, build the model circuits
+    circuits = []
     for i in range(num_circ):
-        # Initialize empty circuit Ci without measurement
-        circuit_i = qp.create_circuit(name, [q], [c])
+        # Initialize empty circuit
+        circuit = QuantumCircuit(q, c)
         # For each layer
         for j in range(depth):
             # Generate uniformly random permutation Pj of [0...n-1]
-            perm = np.random.permutation(n)
-            # For each pair p in Pj, generate Haar random SU(4)
+            perm = random.permutation(n)
+            # For each consecutive pair in Pj, generate Haar random SU(4)
             # Decompose each SU(4) into CNOT + SU(2) and add to Ci
             for k in range(math.floor(n/2)):
                 qubits = [int(perm[2*k]), int(perm[2*k+1])]
                 SU = random_SU(4)
-                for gate in two_qubit_kak(SU):
+                decomposed_SU = two_qubit_kak(SU)
+                for gate in decomposed_SU:
                     i0 = qubits[gate["args"][0]]
                     if gate["name"] == "cx":
                         i1 = qubits[gate["args"][1]]
-                        circuit_i.cx(q[i0], q[i1])
+                        circuit.cx(q[i0], q[i1])
                     elif gate["name"] == "u1":
-                        circuit_i.u1(gate["params"][2], q[i0])
+                        circuit.u1(gate["params"][2], q[i0])
                     elif gate["name"] == "u2":
-                        circuit_i.u2(gate["params"][1], gate["params"][2],
-                                     q[i0])
+                        circuit.u2(gate["params"][1], gate["params"][2], q[i0])
                     elif gate["name"] == "u3":
-                        circuit_i.u3(gate["params"][0], gate["params"][1],
-                                     gate["params"][2], q[i0])
+                        circuit.u3(gate["params"][0], gate["params"][1],
+                                   gate["params"][2], q[i0])
                     elif gate["name"] == "id":
-                        pass  # do nothing
-            # circuit_i.barrier(q)  # barriers between layers
-        circuit_i.barrier(q)  # barrier before measurement
-        # Create circuit with final measurement
-        qp.add_circuit(name, circuit_i + meas)
-    return qp
+                        pass
+        # Barrier before measurement to prevent reordering, then measure
+        circuit.barrier(q)
+        circuit.measure(q, c)
+        # Save sample circuit
+        circuits.append(circuit)
+    return circuits
 
 
 def main():
     parser = argparse.ArgumentParser(
-      description="Create randomized circuits for quantum volume analysis.")
+            description="Create randomized circuits for quantum volume analysis.")
     parser.add_argument('--name', default='quantum_volume',
                         help='circuit name')
     parser.add_argument('-n', '--qubits', default=5, type=int,
@@ -96,43 +90,30 @@ def main():
     parser.add_argument('--num-circ', default=1, type=int,
                         help='how many circuits?')
     parser.add_argument('-r', '--run', action='store_true',
-                        help='execute quantum volume')
-    parser.add_argument('-b', '--backend',
-                        default='local_qasm_simulator', help='backend name')
+                        help='execute circuit(s)')
+    parser.add_argument('-b', '--backend', default='local_qasm_simulator',
+                        help='backend to execute on')
 
     args = parser.parse_args()
 
-    for i in range(args.num_circ):
-        if i == 0:
-            circuit_name = args.name + '_n' + \
-                           str(args.qubits) + '_d'+str(args.depth)
-        else:
-            circuit_name = args.name + str(i) + '_n' + \
-                           str(args.qubits) + '_d' + str(args.depth)
+    circuits = build_model_circuits(n=args.qubits, depth=args.depth,
+                                    num_circ=args.num_circ)
 
-    qp = build_model_circuits(name=circuit_name, n=args.qubits,
-                              depth=args.depth, num_circ=args.num_circ)
-
+    # Run the circuits
     if args.run:
-      backend = args.backend
-      if backend.startswith("ibmqx"):
-        import Qconfig
-        qp.set_api(Qconfig.APItoken, Qconfig.config['url'])
-
-      ret = qp.execute(circuit_name, backend=backend, shots=1,
-                           max_credits=5, hpc=None,
-                           timeout=60*60*24)
-
-      if not ret.get_circuit_status(0) == "DONE":
+        backend_name = args.backend
+        if backend_name.startswith("ibmq"):
+            import Qconfig
+            register(Qconfig.APItoken, Qconfig.config['url'])
+        result = execute(circuits, backend_name=backend_name)
+        print(result.get_counts(circuits[0]))
         return
 
-      sim_result = ret.get_counts(circuit_name)
-      print(sim_result)
-      return
-
-    f = open(circuit_name+'.qasm', 'w')
-    f.write(qp.get_qasm(args.name+'_'+str(i)+'_meas'))
-    f.close()
+    # Save QASM representation of circuits
+    for i in range(num_circ):
+        f = open('quantum_volume_n%d_d%d_i.qasm' % (n, depth, i), 'w')
+        f.write(circuits[i].qasm())
+        f.close()
 
 
 if __name__ == "__main__":
