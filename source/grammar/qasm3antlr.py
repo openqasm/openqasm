79459d523085dc7112b7758ff3311b701f0d651c
Zachary Schoenfeld <zachary.schoenfeld@ibm.com>
2020-12-15 12:43:33 -0500

    Using only lisp parser

--- source/grammar/qasm3antlr.py
+++ source/grammar/qasm3antlr.py
@@ -7,35 +7,6 @@ from qasm3Listener import qasm3Listener
 from antlr4.tree.Trees import Trees
 import re
 
-def pprint_lisp_tree(lisp_tree):
-    indent = 0
-    ltree = lisp_tree[1:len(lisp_tree)-1]
-    ltree = ltree.replace('(', ' ( ')
-    ltree = ltree.replace(')', ' ) ')
-    ltree = ltree.split(' ')
-    print("program")
-    for token in ltree:
-        print(' '*indent, end='')
-        if token == '(':
-            indent += 1
-            print()
-        elif token == ')':
-
-    import pdb; pdb.set_trace()
-
-    """
-    for i, char in enumerate(lisp_tree):
-        if char == '(':
-            if i != 0:  # skip first (
-                indent += 1
-                print('\n'+' '*indent, end='')
-        elif char == ')':
-            if i != len(lisp_tree) - 1:  # skip last )
-                indent -= 1
-        else:
-            print(char, end='')
-    """
-
 def main(argv):
     input = FileStream(argv[1])
     lexer = qasm3Lexer(input)
@@ -43,8 +14,6 @@ def main(argv):
     parser = qasm3Parser(stream)
     tree = parser.program()
     lisp_tree = Trees.toStringTree(tree, None, parser)
-    import pdb; pdb.set_trace()
-    pprint_lisp_tree(lisp_tree)
     import pdb; pdb.set_trace()
 
 if __name__ == '__main__':
