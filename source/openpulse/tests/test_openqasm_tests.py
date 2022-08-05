78f419888e6fa658fb5ae1819e248e5b5ff7c811
Li Chen <dotneteer@gmail.com>
2022-08-05 16:12:29 -0700

    Update source/openpulse/tests/test_openqasm_tests.py
    Co-authored-by: Thomas Alexander <thomasalexander2718@gmail.com>

--- source/openpulse/tests/test_openqasm_tests.py
+++ source/openpulse/tests/test_openqasm_tests.py
@@ -5,7 +5,7 @@ import openpulse
 def test_examples(example_file):
     """Loop through all example files, verify that the ast_parser can parse the file.
 
-    The `example_file` fixture is generated in `conftest.py`.  These tests are automatically skipped
+    The `example_file` fixture is generated in `conftest.py`. These tests are automatically skipped
     if the examples directly cannot be found.
     """
     with open(example_file) as f:
