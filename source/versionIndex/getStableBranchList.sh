f7e37ac3f4bb80149d17ad7d5444988ce382b22a
Michael Healy <mbhealy@ibm.com>
2023-02-28 07:37:41 -0500

    Remove versionIndex

--- source/versionIndex/getStableBranchList.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-git branch --list 'stable/*' `# list all stable branches` |
-sort -r `# sort in reverse order so most recent is first` |
-sed 's/\*//' `# remove the * before the current branch` |
-sed 's/^\s*stable\///g' `# remove leading spaces and stable/` |
-awk '{print "\n`" $0 " <versions/" $0 "/index.html>`__"}' # convert to link
--- source/versionIndex/index.rst
+++
@@ -1,4 +0,0 @@
-OpenQASM Specification Versions
-===============================
-
-`Live Version <versions/live/index.html>`_
