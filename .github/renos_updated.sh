c97b52b7564f2e1cec1e9b65a06c9f0a98301f9a
Thomas Alexander <thomas.a.alexander@ibm.com>
2024-01-11 12:32:16 -0400

    Move debug.

--- .github/renos_updated.sh
+++ .github/renos_updated.sh
@@ -2,6 +2,8 @@
 # This script makes sure a reno has been updated in the PR.
 
 CHANGED_FILES=$(git diff --name-only origin/main $GITHUB_SHA)
+echo Changed files
+echo $CHANGED_FILES
 for file in $CHANGED_FILES
 do
    root=$(echo "./$file" | awk -F/ '{print FS $2}' | cut -c2-)
@@ -9,9 +11,6 @@ do
        exit 0;
    fi
 done
-
-echo Changed files
-echo $CHANGED_FILES
 
 # Place after the diff as reno linting can change the files
 reno lint
