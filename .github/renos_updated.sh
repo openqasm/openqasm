aea7c4c2bed291aca88a4e284542a36c5bdfecb2
Thomas Alexander <thomas.a.alexander@ibm.com>
2024-01-11 12:38:56 -0400

    Remove debugging code.

--- .github/renos_updated.sh
+++ .github/renos_updated.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # This script makes sure a reno has been updated in the PR.
 
+reno lint
+
 CHANGED_FILES=$(git diff --name-only origin/main HEAD)
-echo Changed files
-echo $CHANGED_FILES
 for file in $CHANGED_FILES
 do
    root=$(echo "./$file" | awk -F/ '{print FS $2}' | cut -c2-)
@@ -11,9 +11,6 @@ do
        exit 0;
    fi
 done
-
-# Place after the diff as reno linting can change the files
-reno lint
 
 echo Please add or update a release note in ./releasenotes >&2
 exit 1
