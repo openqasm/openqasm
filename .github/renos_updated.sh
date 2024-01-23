2e1ee240d842725c1e704753112757e2d69c916b
Erik Davis <ejdavis@amazon.com>
2024-01-23 09:23:28 -0800

    reno: only consider changes to `source` directory

--- .github/renos_updated.sh
+++ .github/renos_updated.sh
@@ -3,8 +3,15 @@
 
 git fetch origin main
 
-CHANGED_FILES=$(git diff --name-only origin/main $GITHUB_SHA)
-for file in $CHANGED_FILES
+# We only require release notes for changes in the `source` directory.
+# Other stuff (governance info, github actions, etc) can be ignored.
+CHANGED_SOURCE_FILES=$(git diff --name-only origin/main $GITHUB_SHA -- source)
+if [ -z $CHANGED_SOURCE_FILES ]; then
+    echo "No changes to `source` detected"
+    exit 0;
+fi;
+
+for file in $CHANGED_SOURCE_FILES
 do
    root=$(echo "./$file" | cut -d / -f 3 )
    if [ "$root" = "notes" ]; then
